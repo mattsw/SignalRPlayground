835216aee12f3eff903b9de859612c43decda78a
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-08-08 20:55:42 -0700

    Added Test project for usertools so TDD can begin

---
+++ SignalRPlayground/SignalRPlayground.UserTools.Tests/UserManagerAdapterTests.cs
@@ -0,0 +1,26 @@
+﻿using SignalRPlayground.UserTools.AncientUserManager;
+using SignalRPlayground.UserTools.Interfaces;
+
+namespace SignalRPlayground.UserTools.Tests;
+
+[TestFixture]
+public class UserManagerAdapterTests
+{
+
+    [Test]
+    public void UserManagerAdapter_ShouldInstantiateWithOldManager()
+    {
+        var sut = new UserManagerAdapter(new OldUserManager());
+        
+        Assert.That(sut, Is.Not.Null);
+    }
+
+    [Test]
+    public void UserManagerAdapter_ShouldWorkWithNewInterface()
+    {
+        var sut = new UserManagerAdapter(new OldUserManager());
+        
+        Assert.That(sut, Is.Not.Null);
+        Assert.That(sut, Is.InstanceOf<IUserManager>());
+    }
+}
