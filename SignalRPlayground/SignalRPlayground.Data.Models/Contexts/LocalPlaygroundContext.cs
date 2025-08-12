5623cf9ec45a8744e668481ba4ab0f0c391e7dcd
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-08-12 06:07:21 -0700

    Added working front to back for usercontroller put method

--- SignalRPlayground/SignalRPlayground.Data.Models/Contexts/LocalPlaygroundContext.cs
+++ SignalRPlayground/SignalRPlayground.Data.Models/Contexts/LocalPlaygroundContext.cs
@@ -5,12 +5,5 @@ using Microsoft.EntityFrameworkCore;
 
 public class LocalPlaygroundContext(DbContextOptions<LocalPlaygroundContext> options) : DbContext(options)
 {
-    //TODO move this to appsettings/secrets though it's fine for initial dev since there's nothing super scary
-    private const string ConnectionString = "data source=DESKTOP-TSFDMTT;initial catalog=LocalPlayground;trusted_connection=true;TrustServerCertificate=true;";
     public DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(ConnectionString);
-    }
 }
--- SignalRPlayground/SignalRPlayground.Web/Program.cs
+++ SignalRPlayground/SignalRPlayground.Web/Program.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
 using SignalRPlayground.Data.Models.Contexts;
 using SignalRPlayground.Repositories.Users;
 using SignalRPlayground.UserTools.UserManager;
@@ -6,10 +8,12 @@ using SignalRPlayground.Web.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("LocalPlayground");
 
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<LocalPlaygroundContext>();
+builder.Services.AddDbContext<LocalPlaygroundContext>(options => 
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<INewUserManager, NewUserManager>();
